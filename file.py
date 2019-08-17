b795faf6fd4c34c86ef76f4ae35cb05a4142da1d
ioanaMMT <ioana2morosanu@gmail.com>
2019-08-17 14:36:18 +0100

    New update

--- file.py
+++ file.py
@@ -5,7 +5,7 @@ books = [
     "Fluent Python: Clear, Concise, and Effective Programming - Luciano Ramalho",
     "Python for Kids: A Playful Introduction To Programming - Jason R. Briggs",
     "Hello Web App: Learn How to Build a Web App - Tracy Osborn",
-    "The last one"
+    "I did it!!!!"
 ]
 
 video_games = [
